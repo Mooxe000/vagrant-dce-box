4cd79bd3a12f1c15ea918785d190506269913e13
FooTearth <footearth@gmail.com>
2016-12-07 11:29:03 +0800

    pull all images for that can install DCE faster

--- scripts/common/dce.sh
+++ scripts/common/dce.sh
@@ -1,7 +1,19 @@
 #!/bin/sh -eux
 
-docker pull \
-  daocloud.io/daocloud/dce:2.2.0
+dce_version="2.2.0"
+dce_namespace="daocloud.io/daocloud"
+
+for image in \
+  "dce" \
+  "dce-controller" \
+  "dce-etcd" \
+  "dce-swarm" \
+  "dce-agent";
+do
+  docker pull \
+    "${dce_namespace}/${image}:${dce_version}"
+done
+
 docker tag \
   daocloud.io/daocloud/dce:2.2.0 \
   daocloud.io/daocloud/dce:latest
